26d17cc2e8a4c1ec1570518b8932623e8c31f8d3
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-17 19:45:44 +0300

    complete js function

--- divColor-Change/dic_color_change.js
+++ divColor-Change/dic_color_change.js
@@ -1,5 +1,13 @@
-
-// change div theme color. 
+// change div theme color.
+var counter = 0;
 function changeTheme() {
-    
+  const colors = ["red", "green", "black"];
+
+  if (counter < 3) {
+    document.getElementById("contents").style.backgroundColor = colors[counter];
+    counter++;
+  } else {
+    counter = 0;
+  }
+  console.log(counter);
 }
