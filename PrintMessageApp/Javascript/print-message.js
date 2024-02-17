d22bbd6c6f2811edfb8538f153c43e308d2de1c8
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-17 19:03:10 +0300

    added validation to form

--- PrintMessageApp/Javascript/print-message.js
+++ PrintMessageApp/Javascript/print-message.js
@@ -1,7 +1,15 @@
 function printMessage() {
   let message = document.getElementById("input").value;
-  document.getElementById("message").innerHTML = message;
 
+  if (message === null || message === "") {
+    document.getElementById("error-msg").innerHTML = "Please enter a value";
+    document.getElementById("error-msg").style.color = "red";
+    document.getElementById("error-msg").style.fontSize = 16;
+    document.getElementById("message").innerHTML = "";
+  } else {
+    document.getElementById("message").innerHTML = message;
+    document.getElementById("error-msg").innerHTML = "";
+  }
   let reset = document.getElementById("input");
   reset.value = null;
 }
