45fd60d445b95c4629b0d143ee7d48569142f93f
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-17 19:09:46 +0300

    complete print message app

--- PrintMessageApp/Javascript/print-message.js
+++ PrintMessageApp/Javascript/print-message.js
@@ -2,7 +2,7 @@ function printMessage() {
   let message = document.getElementById("input").value;
 
   if (message === null || message === "") {
-    document.getElementById("error-msg").innerHTML = "Please enter a value";
+    document.getElementById("error-msg").innerHTML = "Field is requred";
     document.getElementById("error-msg").style.color = "red";
     document.getElementById("error-msg").style.fontSize = 16;
     document.getElementById("message").innerHTML = "";
