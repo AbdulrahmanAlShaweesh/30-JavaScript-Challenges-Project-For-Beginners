f1496c90e87026a2caf7b4defd6d7000ae6e5a59
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2024-02-22 22:32:41 +0300

    complted project

---
+++ randomBodyBackground/random_body_background.js
@@ -0,0 +1,15 @@
+let body = document.body.style;
+
+const colors = ["red", "green", "blue", "black", "pink", "skyblue"];
+
+function radomBodyBackground() {
+  body.backgroundColor = colors[Math.floor(Math.random(0, colors.length+1) * colors.length)];
+  
+  setTimeout(function () {
+    radomBodyBackground();
+  }, 2000);
+  let x = Math.floor(Math.random(0, colors.length) * colors.length+1);
+  console.log(x);
+}
+
+radomBodyBackground();
